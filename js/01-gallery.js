9eba1280f5c862cfb62eb8a899fd5fede64ff38d
Marta Żak <martysik19@gmail.com>
2022-02-05 21:47:54 +0100

    added create gallery markup function and prettier config

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+function addGalleryMarkup(images) {
+  const markup = images.map(image => createGalleryMarkup(image)).join('');
+
+  return (galleryRef.innerHTML = markup);
+}
+
+function createGalleryMarkup(image) {
+  const { original, preview, description } = image;
+
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+}
+
+addGalleryMarkup(galleryItems);
